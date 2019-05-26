a8c546e9991f525ce9b7a2ae267782f96a19c04c
jovan <jovan.joksi@hotmail.com>
2019-05-26 23:38:51 +0200

    modify seed data

--- Emit.Web/Program.cs
+++ Emit.Web/Program.cs
@@ -42,15 +42,21 @@ namespace Emit.Web
                 context.Roles.Add(admin);
                 context.Roles.Add(menager);
                 context.Roles.Add(employee);
-                List<User> users = new List<User>
+                List<User> admins = new List<User>
                    {
                        new User("admin.user@hotmail.com","password"),
                        new User("admin.user2@hotmail.com","password"),
-                       new User("admin.user3@hotmail.com","password"),
-                       new User("menager.user1@hotmail.com","password"),
+                       new User("admin.user3@hotmail.com","password")
+                };
+                List<User> menagers = new List<User>()
+                {
+                        new User("menager.user1@hotmail.com","password"),
                        new User("menager.user2@hotmail.com","password"),
                        new User("menager.user3@hotmail.com","password"),
                        new User("menager.user4@hotmail.com","password"),
+                };
+                List<User> regular = new List<User>()
+                {
                        new User("regular.user1@hotmail.com","password"),
                        new User("regular.user2@hotmail.com","password"),
                        new User("regular.user3@hotmail.com","password"),
@@ -61,28 +67,49 @@ namespace Emit.Web
                        new User("regular.user8@hotmail.com","password"),
                        new User("regular.user9@hotmail.com","password"),
                        new User("regular.user10@hotmail.com","password"),
-                  };
+                };
+                List<Sector> ssectors = new List<Sector>();
                 var sectors = new List<string>
                 {
                     "IT","Marketing","Menagment","Business"
                 };
                 int menId = 0;
-                foreach (var user in users)
+                foreach (var user in admins)
                 {
-                    if (user.Email.Contains("admin"))
-                        admin.AddUser(user);
-                    if (user.Email.Contains("menager"))
-                    {
-                        menager.AddUser(user);
-                        context.Sectors.Add(new Sector(sectors[menId], user));
-                        menId++;
-                    }
-                    if (user.Email.Contains("regular"))
-                        employee.AddUser(user);
+                    admin.AddUser(user);
                     user.IsEmailConfirmed = true;
                 }
-                context.Users.AddRange(users);
+                foreach (var user in menagers)
+                {
+                    menager.AddUser(user);
+                    var s = new Sector(sectors[menId], user);
+                    context.Sectors.Add(s);
+                    ssectors.Add(s);
+                    menId++;
+                    user.IsEmailConfirmed = true;
+
+                }
+                menId = menagers.Count - 1;
+                foreach (var user in regular)
+                {
+                    menId = menId % menagers.Count;
+                    employee.AddUser(user);
+                    user.IsEmailConfirmed = true;
+                    ssectors[menId].Employees.Add(user);
+                    menId++;
+                }
+                context.Users.AddRange(admins);
+                context.Users.AddRange(menagers);
+                context.Users.AddRange(regular);
                 context.SaveChanges();
+                menId = 0;
+                foreach(var sec in ssectors)
+                {
+                    sec.Employees.Add(menagers[menId]);
+                        menId++;
+                }
+                context.SaveChanges();
+
             }
         }
 
